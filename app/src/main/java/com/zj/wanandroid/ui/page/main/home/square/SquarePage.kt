425404a31ab4662600a146454b1bbd82d2737c30
shenzhen2017 <2868405029@qq.com>
2021-11-19 08:56:50 +0800

    ADD: SquarePage

--- app/src/main/java/com/zj/wanandroid/ui/page/main/home/square/SquarePage.kt
+++ app/src/main/java/com/zj/wanandroid/ui/page/main/home/square/SquarePage.kt
@@ -1,10 +1,19 @@
 package com.zj.wanandroid.ui.page.main.home.square
 
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.ScaffoldState
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavHostController
+import androidx.paging.compose.collectAsLazyPagingItems
+import androidx.paging.compose.itemsIndexed
+import com.google.accompanist.swiperefresh.SwipeRefresh
+import com.google.accompanist.swiperefresh.rememberSwipeRefreshState
+import com.zj.wanandroid.ui.widgets.MultiStateItemView
 
 @Composable
 fun SquarePage(
@@ -12,5 +21,51 @@ fun SquarePage(
     scaffoldState: ScaffoldState,
     viewModel: SquareViewModel = hiltViewModel()
 ) {
-    Text(text = "广场")
+    val squareData = viewModel.viewStates.pagingData?.collectAsLazyPagingItems()
+    val refreshing: Boolean = viewModel.viewStates.isRefreshing
+    val swipeRefreshState = rememberSwipeRefreshState(refreshing)
+
+    SwipeRefresh(
+        state = swipeRefreshState,
+        onRefresh = { //viewModel.refresh()
+        }
+    ) {
+
+        LazyColumn(
+            modifier = Modifier.fillMaxSize(),
+            contentPadding = PaddingValues(top = 10.dp)
+        ) {
+            if (squareData!!.itemCount > 0) {
+                itemsIndexed(squareData) { index, item ->
+                    MultiStateItemView(
+                        data = item!!,
+                        onSelected = {
+//                            viewModel.saveDataToHistory(item)
+//                            viewModel.savePosition(listState.firstVisibleItemIndex)
+//                            RouteUtils.navTo(navCtrl, RouteName.WEB_VIEW, it)
+                        },
+                        onCollectClick = {
+//                            if (item.collect) {
+//                                viewModel.uncollectArticleById(it)
+//                                squareData.peek(index)?.collect = false
+//                            } else {
+//                                viewModel.collectArticleById(it)
+//                                squareData.peek(index)?.collect = true
+//                            }
+
+                        },
+                        onUserClick = { userId ->
+//                            RouteUtils.navTo(navCtrl, RouteName.SHARER, userId)
+                        })
+                }
+//                onScrollChangeListener(listState.firstVisibleItemIndex)
+            } else {
+                item {
+//                    EmptyView()
+                }
+            }
+
+
+        }
+    }
 }
--- app/src/main/java/com/zj/wanandroid/ui/page/main/home/square/SquareViewModel.kt
+++ app/src/main/java/com/zj/wanandroid/ui/page/main/home/square/SquareViewModel.kt
@@ -1,11 +1,17 @@
 package com.zj.wanandroid.ui.page.main.home.square
 
 import android.util.Log
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.zj.wanandroid.data.http.HttpService
+import androidx.paging.PagingData
 import com.zj.wanandroid.common.paging.simplePager
+import com.zj.wanandroid.data.bean.Article
+import com.zj.wanandroid.data.http.HttpService
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.flow.onEach
@@ -16,12 +22,15 @@ import javax.inject.Inject
 class SquareViewModel @Inject constructor(
     private var service: HttpService,
 ) : ViewModel() {
+    var viewStates by mutableStateOf(SquareViewState())
+        private set
+
     private val pager by lazy {
         simplePager { service.getSquareData(it) }
     }
 
     init {
-        fetchData()
+        viewStates = viewStates.copy(pagingData = pager)
     }
 
     private fun fetchData() {
@@ -34,6 +43,11 @@ class SquareViewModel @Inject constructor(
             }.collect()
         }
     }
-
 }
 
+data class SquareViewState(
+    val isRefreshing: Boolean = false,
+    val pagingData: PagingArticle? = null
+)
+
+typealias PagingArticle = Flow<PagingData<Article>>
